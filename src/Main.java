891e05da9b68af35468845c01d318338b147d6de
vedranbasic <vedran.basic98@gmail.com>
2026-02-16 19:22:18 +0100

    chore: rijesenje prvog zadatka - isDigit, isLetter i other

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,29 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Pozdrav!");
+        Scanner unos = new Scanner(System.in);
+        String uneseno = unos.nextLine();
+
+        int brojevi = 0;
+        int slova = 0;
+        int ostalo = 0;
+
+        for (int i = 0; i < uneseno.length(); i++) {
+            char character = uneseno.charAt(i);
+            if (Character.isDigit(character)) {
+                brojevi++;
+            } else if(Character.isLetter(character)) {
+                slova++;
+            } else {
+                ostalo++;
+            }
+        }
+
+        System.out.println("brojevi: " + brojevi);
+        System.out.println("slova: " + slova);
+        System.out.println("ostalo: " + ostalo);
     }
 }
