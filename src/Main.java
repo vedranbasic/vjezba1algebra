32b3bd119d2b4988d59d736d74ee6cc85496b208
vedranbasic <vedran.basic98@gmail.com>
2026-02-16 19:26:35 +0100

    chore: obtanje rijeci u recenici

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,16 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Pozdrav!");
+        Scanner unos = new Scanner(System.in);
+        String uneseno = unos.nextLine();
+
+        String[] rijeci = uneseno.split(" ");
+        for (int i = rijeci.length -1; i >= 0; i--) {
+            System.out.print(rijeci[i] + " ");
+        }
+
     }
 }
