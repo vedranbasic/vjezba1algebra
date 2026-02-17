1d46447ca4be3af242144fa4bfce7a0a8dcdf054
vedranbasic <vedran.basic98@gmail.com>
2026-02-17 19:15:20 +0100

    chore: fizz buzz

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,15 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Pozdrav!");
+        for (int i = 1; i <= 100; i++) {
+            StringBuilder s = new StringBuilder();
+            if (i%3==0) {
+                s.append("Fizz");
+            }
+            if (i%5==0) {
+                s.append("Buzz");
+            }
+            System.out.println(i + ": " + s.toString());
+        }
     }
 }
