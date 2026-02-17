f359c21accd35918128ae58ea9763cf5dfc41aa9
vedranbasic <vedran.basic98@gmail.com>
2026-02-17 19:12:20 +0100

    chore: pretvarac temperature

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,20 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Pozdrav!");
+        System.out.println("Unesite temperaturu u Celzijusima: ");
+        Scanner sc = new Scanner(System.in);
+        double celzius = sc.nextDouble();
+
+        double fahrenheit = (celzius * 1.8) + 32;
+        double kelvin = celzius + 273.15;
+
+        System.out.println("Fahrenheit: " + fahrenheit);
+        System.out.println("Kelvin: " + kelvin);
+
     }
 }
