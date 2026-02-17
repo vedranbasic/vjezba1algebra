3a9bd40442b2f5d8985c735cc317cc7eda251be5
vedranbasic <vedran.basic98@gmail.com>
2026-02-17 21:24:23 +0100

    fix: malo citljiviji kod - ispravak

--- src/Main.java
+++ src/Main.java
@@ -9,61 +9,18 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         String oib = sc.nextLine();
 
-        // 1. prva znamenka zbroji se s brojem 10
-        int prvaZnamenka = Character.getNumericValue(oib.charAt(0));
-        double ostatakNijeNula = prvaZnamenka + 10;
 
+        int tempbroj = 10;
 
-        for (int i = 0; ; i++) {
-
-            // 2. dobiveni zbroj cjelobrojno (s ostatkom) podijeli se brojem 10; ako je dobiveni
-            // ostatak 0 zamijeni se brojem 10 (ovaj broj je tzv. meduostatak)
-            double meduostatak = ostatakNijeNula % 10;
-            if (meduostatak == 0) {
-                meduostatak = 10;
-            }
-
-            // 3. dobiveni medjuostatak pomnozi se brojem 2
-            double umnozak = meduostatak * 2;
-
-            // 4. dobiveni umnozak cjelobrojno (s ostatkom) podijeli se brojem 11; ovaj ostatak
-            // matematicki nikako ne može biti 0 jer je rezultat prethodnog koraka uvijek paran
-            // broj
-            ostatakNijeNula = umnozak % 11;
-
-            // 5. slijedeća znamenka zbroji se s ostatkom u prethodnom koraku
-            double iducaZnamenka = Character.getNumericValue(oib.charAt(i + 1));
-            ostatakNijeNula = ostatakNijeNula +  iducaZnamenka;
-            if (i == oib.length() - 2) {
-                break;
-            }
+        for (int i = 0; i < oib.length() - 1; i++) {
+            tempbroj = Character.getNumericValue(oib.charAt(i)) + tempbroj;
+            tempbroj = (tempbroj % 10 == 0) ? 10 : (tempbroj % 10);
+            tempbroj *= 2;
+            tempbroj %= 11;
         }
-
-
-        if (ostatakNijeNula == 1) {
-            if (Character.getNumericValue(oib.charAt(oib.length() - 1)) == 0) {
-                System.out.println("Kontrolna znamenka je: 0, OIB je ispravan!");
-            } else {
-                System.out.println("Kontrolna znamenka je: 0, OIB je neispravan!");
-            }
-
-        } else {
-
-            double kontrolnaZnamenka = 11 - ostatakNijeNula;
-            if (kontrolnaZnamenka == Character.getNumericValue(oib.charAt(oib.length() - 1))) {
-                System.out.println("Kontrolna znamenka je: " + kontrolnaZnamenka + ", OIB je ispravan!");
-            } else {
-                System.out.println("Kontrolna znamenka je: " + kontrolnaZnamenka + ", OIB je neispravan!");
-            }
-        }
-
-
-
-
-
-
-
-
-
+        tempbroj = 11 - tempbroj;
+        tempbroj = tempbroj == 10 ? 0 : tempbroj;
+        int kontrolniBroj = Character.getNumericValue(oib.charAt(oib.length() -1));
+        System.out.println(kontrolniBroj == tempbroj);
     }
 }
