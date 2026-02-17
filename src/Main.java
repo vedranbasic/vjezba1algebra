ee0aae6f2920f371899564e9c540fd90936cd5a2
vedranbasic <vedran.basic98@gmail.com>
2026-02-17 19:04:13 +0100

    chore: najveci broj u nizu

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,30 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Pozdrav!");
+        System.out.println("Unesite broj clanova niza");
+        Scanner input = new Scanner(System.in);
+        int brojClanovaNiza = input.nextInt();
+
+        int[] niz = new int[brojClanovaNiza];
+        while (brojClanovaNiza != 0) {
+            Scanner in = new Scanner(System.in);
+            niz[brojClanovaNiza-1] = in.nextInt();
+            brojClanovaNiza--;
+        }
+
+        int najveci = niz[0];
+        for (int i : niz) {
+            if (i > najveci) {
+                najveci = i;
+            }
+        }
+        System.out.println("Najveci: " + najveci);
+
+
     }
 }
