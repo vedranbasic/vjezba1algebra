7500e7752c47bae1ec2bee94408d2ac9e66280e6
vedranbasic <vedran.basic98@gmail.com>
2026-02-17 19:09:28 +0100

    chore: povrsina pravokutnika

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,16 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Pozdrav!");
+        System.out.println("Unesite stranice pravokutnika:");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println("Povrsina je: " + a*b);
+
     }
 }
