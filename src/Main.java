3a40dcd3d0ba45bfe95ef940893822aa9741c6bf
vedranbasic <vedran.basic98@gmail.com>
2026-03-03 20:54:49 +0100

    chore: faktorijeli

--- src/Main.java
+++ src/Main.java
@@ -7,25 +7,38 @@ import java.util.Scanner;
 // ne radi na 48029936706
 public class Main {
     public static void main(String[] args) {
-        Automobil prviAuto = new Automobil("Opel", 50, 4);
-        Automobil drugiAuto = new Automobil("Mercedes", 70, 4);
-        Motocikl prviMotor = new Motocikl("Suzuki", 70, "Dizel");
-        Motocikl drugiMotor = new Motocikl("Harley Davidson", 100, "Benzin");
+        Scanner unos = new Scanner(System.in);
+        String podatakUnosa;
+        do {
+            podatakUnosa = unos.nextLine();
+        } while (!ispravanUnos(podatakUnosa));
 
-        Vozilo[] vozila =  new Vozilo[] {prviAuto,drugiAuto,prviMotor,drugiMotor};
+        try {
+            Integer broj = Integer.parseInt(podatakUnosa);
+            int faktorijel = 1;
+            for (int i = 1; i <= broj; i++) {
+                faktorijel *= i;
+            }
 
-        for (Vozilo vozilo : vozila) {
-            System.out.println(vozilo.toString());
+            System.out.println("Faktorijel broja " + broj  + " je " + faktorijel);
+        } catch (NumberFormatException e) {
+            System.out.println("Pogreska kod ucitavanja podatka: " + e.getMessage());
         }
 
-        for (Vozilo vozilo : vozila) {
-            vozilo.ubrzaj(5);
-            System.out.println(vozilo.toString());
+    }
+
+    public static boolean ispravanUnos(String unos) {
+        Integer broj;
+        try {
+            broj = Integer.parseInt(unos);
+            if (broj < 0) {
+                throw new NumberFormatException("Broj je negativan!");
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Pogreška kod učitavanja podatka: " + e.getMessage());
+            return false;
         }
-
-
-
-
+        return true;
     }
 
 }
--- src/Ubrzanje.java
+++
@@ -1,3 +0,0 @@
-public interface Ubrzanje {
-    void ubrzaj(int ubrzanje);
-}
