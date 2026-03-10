242f05736b24b7c3e70bf2c9ae9b53a7d209b2c4
vedranbasic <vedran.basic98@gmail.com>
2026-03-10 20:17:57 +0100

    chore: ciscenje koda

--- src/Main.java
+++ src/Main.java
@@ -9,36 +9,9 @@ public class Main {
     public static void main(String[] args) {
         Scanner unos = new Scanner(System.in);
         String podatakUnosa;
-        do {
-            podatakUnosa = unos.nextLine();
-        } while (!ispravanUnos(podatakUnosa));
+        podatakUnosa = unos.nextLine();
+        System.out.println(podatakUnosa);
 
-        try {
-            Integer broj = Integer.parseInt(podatakUnosa);
-            int faktorijel = 1;
-            for (int i = 1; i <= broj; i++) {
-                faktorijel *= i;
-            }
-
-            System.out.println("Faktorijel broja " + broj  + " je " + faktorijel);
-        } catch (NumberFormatException e) {
-            System.out.println("Pogreska kod ucitavanja podatka: " + e.getMessage());
-        }
-
-    }
-
-    public static boolean ispravanUnos(String unos) {
-        Integer broj;
-        try {
-            broj = Integer.parseInt(unos);
-            if (broj < 0) {
-                throw new NumberFormatException("Broj je negativan!");
-            }
-        } catch (NumberFormatException e) {
-            System.out.println("Pogreška kod učitavanja podatka: " + e.getMessage());
-            return false;
-        }
-        return true;
     }
 
 }
